653089acef92b40b09f3bec1169f47e6fb6d1421
visualstudio <visualstudio@122-PC30>
2019-01-30 12:17:51 -0500

    village paths

--- c0727454--week--04/Program.cs
+++ c0727454--week--04/Program.cs
@@ -4,26 +4,76 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace c0727454__week__04
+namespace C0723381_WEEK_04
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Countryside blue = new Countryside();
+            blue.Launch();
         }
     }
 
-
-
-    class Node
+    class Village
     {
+        // Node is an ADT
+        // what KIND of DATA do we need in a nood?
+        public Village nextVillage;
+        public Village previousVillage;
+        public String VillageName;
+        public bool isAstrildeHere = false;
 
-        // Node is an adt
-        //what kind of data do we need in a Node?
-        public Node nextvillage;
-        public string pohir;
-        public bool isAstrildHere = false;
+    }
+    class Countryside
+    {
+        Village Maple = new Village();
+
+        Village Toronto = new Village();
+
+        Village Ajax = new Village();
+        Village First;
+        Village Last;
+        Village Temp;
+
+
+        public void Launch()
+        {
+            Village Temp;
+            First = Maple;
+            Last = Ajax;
+            Maple.VillageName = "Maple";
+            Maple.nextVillage = Toronto;
+            Maple.previousVillage = null;
+            Toronto.VillageName = "Toronto";
+            Toronto.nextVillage = Ajax;
+            Toronto.nextVillage = Maple;
+            Ajax.VillageName = "Ajax";
+            Ajax.nextVillage = null;
+            Ajax.nextVillage = Toronto;
+
+            Console.WriteLine(this.displayMap());
+            Console.ReadLine();
+
+
+        }
+        public string displayMap()
+        {
+            string listOfCities = "";
+            // we need to print all the cities
+
+
+            listOfCities = listOfCities + First.VillageName;
+            Temp = First.nextVillage;
+            listOfCities = listOfCities + Temp.VillageName;
+
+
+            return listOfCities;
+        }
+
 
 
     }
 }
+
+
